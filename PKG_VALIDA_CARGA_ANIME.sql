e665cd345ac056fedae23bb1273868a55f49d755
matheus <cardosomcds1@gmail.com>
2023-06-23 17:41:13 -0300

    Leitura e exportação dos csv

--- PKG_VALIDA_CARGA_ANIME.sql
+++ PKG_VALIDA_CARGA_ANIME.sql
@@ -1,155 +1,96 @@
 --PACKAGE QUE VAI VALIDAR AS ALTERACOES NA CARGA DO ARQUIVO ANIME.CSV
-create or replace  PACKAGE PKG_VALIDA_CARGA_ANIME IS
+create or replace  PACKAGE PKG_VALIDA_CARGA IS
     TYPE TP_DATAS IS RECORD (MAL_ID NUMBER,
                              DT_INICIAL VARCHAR2(100),
                              DT_FINAL VARCHAR2(100));
     TYPE TB_DATAS IS TABLE OF TP_DATAS; 
 
-    PROCEDURE SP_ALTERA_INDEFINIDOS_PARA_NULOS;
-    
-    -- VALIDAR DATAS
-    FUNCTION FNC_VALIDA_DATAS_INICIAIS_FINAIS (PLETRAS NUMBER) RETURN TB_DATAS PIPELINED;
-    PROCEDURE SP_DATAS_INICIAIS_FINAIS;
+PROCEDURE SP_VALIDA_VALROES_NULO;
+
+-- VALIDAR DATAS
+FUNCTION FNC_VALIDA_DATAS_INICIAIS_FINAIS RETURN TB_DATAS PIPELINED;
+PROCEDURE SP_DATAS_INICIAIS_FINAIS;
 END;
 
 
-
 -- CORPO DA PACKAGE
-CREATE OR REPLACE PACKAGE BODY PKG_VALIDA_CARGA_ANIME IS
-  PROCEDURE SP_ALTERA_INDEFINIDOS_PARA_NULOS AS 
-    -- TRATAMENTO DE VALORES NULOS POR COLUNA
-    VNULO VARCHAR2(20) := NULL;
-    BEGIN
-    -- TRATAMENTO DE VALORES NULOS
-        UPDATE CARGA_ANIME
-        SET "English name" = VNULO
-        WHERE "English name" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Premiered" = 'NULL'
-        WHERE "Premiered" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Episodes" = VNULO
-        WHERE "Episodes" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Type" = VNULO
-        WHERE "Type" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Aired" = VNULO
-        WHERE "Aired" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Name" = VNULO
-        WHERE "Name" IN ('Unknown', 'NDA', 'NULL','9','86');
-        
-        UPDATE CARGA_ANIME
-        SET "Score" = VNULO
-        WHERE "Score" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Genres" = VNULO
-        WHERE "Genres" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Licensors" = VNULO
-        WHERE "Licensors" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Studios" = VNULO
-        WHERE "Studios" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Genres" = VNULO
-        WHERE "Genres" IN ('Unknown', 'NDA', 'NULL');
-        
-        UPDATE CARGA_ANIME
-        SET "Source" = VNULO
-        WHERE "Source" IN ('Unknown', 'NDA', 'NULL');        
-        
-        UPDATE CARGA_ANIME
-        SET "Source" = VNULO
-        WHERE "Source" IN ('Unknown', 'NDA', 'NULL');        
-        
-        UPDATE CARGA_ANIME
-        SET "Duration" = VNULO
-        WHERE "Duration" IN ('Unknown', 'NDA', 'NULL');  
-        
-        UPDATE CARGA_ANIME
-        SET "Ranked" = VNULO
-        WHERE "Ranked" IN ('Unknown', 'NDA', 'NULL');          
-        
-    COMMIT;
-    
+create or replace NONEDITIONABLE PACKAGE BODY PKG_VALIDA_CARGA IS
+
+    PROCEDURE SP_VALIDA_VALORES_NULO AS 
+        -- TRATAMENTO DE VALORES NULOS PARA AS COLUNAS DE TABELAS DE CARGA 'CG_'
+    BEGIN    
+        FOR I IN (SELECT TABLE_NAME, '"'||COLUMN_NAME||'"' AS COLUMN_NAME FROM ALL_TAB_COLUMNS WHERE TABLE_NAME  LIKE 'CG_%') LOOP
+            EXECUTE IMMEDIATE 'UPDATE '|| I.TABLE_NAME ||
+                                ' SET '|| I.COLUMN_NAME || '= NULL 
+                                WHERE ' || I.COLUMN_NAME ||' IN (''Unknown'')';
+        END LOOP;
+        COMMIT;
     END;
+
     
-    FUNCTION FNC_VALIDA_DATAS_INICIAIS_FINAIS (PLETRAS NUMBER) RETURN TB_DATAS PIPELINED AS
-        VREGISTRO PKG_VALIDA_CARGA_ANIME.TP_DATAS;
-     -- O LOOP NÃO REALIZADO PELA CONSULTA JÃ? TRAZENDOS A DATA INICIAL E FINAL TRATADA E DEPOÃ?S Ã‰ RETORNADA EM UM TABELA 
-     BEGIN
-        FOR I IN (SELECT 
-                    "MAL_ID",
-                    CASE WHEN LENGTH(DATA_FINAL)> PLETRAS THEN
-                            TO_CHAR(TO_DATE(DECODE(substr(DATA_FINAL,1,3),
-                                                   'Jan', '01'||substr(DATA_FINAL,4),
-                                                   'Feb', '02'||substr(DATA_FINAL,4),
-                                                   'Mar', '03'||substr(DATA_FINAL,4),
-                                                   'Apr', '04'||substr(DATA_FINAL,4),
-                                                   'May', '05'||substr(DATA_FINAL,4),
-                                                   'Jun', '06'||substr(DATA_FINAL,4),
-                                                   'Jul', '07'||substr(DATA_FINAL,4),
-                                                   'Aug', '08'||substr(DATA_FINAL,4),
-                                                   'Sep', '09'||substr(DATA_FINAL,4),
-                                                   'Oct', '10'||substr(DATA_FINAL,4),
-                                                   'Nov', '11'||substr(DATA_FINAL,4),
-                                                   'Dec', '12'||substr(DATA_FINAL,4)),'MM DD YYYY'),'DD/MM/YYYY') 
-                         ELSE NULL END AS DATA_FINAL,
-                    CASE WHEN LENGTH(DATA_INICIAL)> PLETRAS THEN
-                                            TO_CHAR(TO_DATE(DECODE(substr(DATA_INICIAL,1,3),
-                                                   'Jan', '01'||substr(DATA_INICIAL,4),
-                                                   'Feb', '02'||substr(DATA_INICIAL,4),
-                                                   'Mar', '03'||substr(DATA_INICIAL,4),
-                                                   'Apr', '04'||substr(DATA_INICIAL,4),
-                                                   'May', '05'||substr(DATA_INICIAL,4),
-                                                   'Jun', '06'||substr(DATA_INICIAL,4),
-                                                   'Jul', '07'||substr(DATA_INICIAL,4),
-                                                   'Aug', '08'||substr(DATA_INICIAL,4),
-                                                   'Sep', '09'||substr(DATA_INICIAL,4),
-                                                   'Oct', '10'||substr(DATA_INICIAL,4),
-                                                   'Nov', '11'||substr(DATA_INICIAL,4),
-                                                   'Dec', '12'||substr(DATA_INICIAL,4)),'MM DD YYYY'),'DD/MM/YYYY') 
-                         ELSE NULL END AS DATA_INICIAL 
+    FUNCTION FNC_VALIDA_DATAS_INICIAIS_FINAIS RETURN TB_DATAS PIPELINED IS
+        VREGISTRO PKG_VALIDA_CARGA.TP_DATAS;
+        BEGIN
+        FOR I IN (SELECT "MAL_ID",
+                            CASE WHEN LENGTH(DATA_FINAL)> 9 THEN
+                                TO_CHAR(TO_DATE(DECODE(substr(DATA_FINAL,1,3),
+                                                        'Jan', '01'||substr(DATA_FINAL,4),
+                                                        'Feb', '02'||substr(DATA_FINAL,4),
+                                                        'Mar', '03'||substr(DATA_FINAL,4),
+                                                        'Apr', '04'||substr(DATA_FINAL,4),
+                                                        'May', '05'||substr(DATA_FINAL,4),
+                                                        'Jun', '06'||substr(DATA_FINAL,4),
+                                                        'Jul', '07'||substr(DATA_FINAL,4),
+                                                        'Aug', '08'||substr(DATA_FINAL,4),
+                                                        'Sep', '09'||substr(DATA_FINAL,4),
+                                                        'Oct', '10'||substr(DATA_FINAL,4),
+                                                        'Nov', '11'||substr(DATA_FINAL,4),
+                                                        'Dec', '12'||substr(DATA_FINAL,4)),'MM DD YYYY'),'DD/MM/YYYY') 
+                                ELSE NULL END AS DATA_FINAL,
+                            CASE WHEN LENGTH(DATA_INICIAL)> 9 THEN
+                                                TO_CHAR(TO_DATE(DECODE(substr(DATA_INICIAL,1,3),
+                                                        'Jan', '01'||substr(DATA_INICIAL,4),
+                                                        'Feb', '02'||substr(DATA_INICIAL,4),
+                                                        'Mar', '03'||substr(DATA_INICIAL,4),
+                                                        'Apr', '04'||substr(DATA_INICIAL,4),
+                                                        'May', '05'||substr(DATA_INICIAL,4),
+                                                        'Jun', '06'||substr(DATA_INICIAL,4),
+                                                        'Jul', '07'||substr(DATA_INICIAL,4),
+                                                        'Aug', '08'||substr(DATA_INICIAL,4),
+                                                        'Sep', '09'||substr(DATA_INICIAL,4),
+                                                        'Oct', '10'||substr(DATA_INICIAL,4),
+                                                        'Nov', '11'||substr(DATA_INICIAL,4),
+                                                        'Dec', '12'||substr(DATA_INICIAL,4)),'MM DD YYYY'),'DD/MM/YYYY') 
+                                ELSE NULL END AS DATA_INICIAL 
                     
-                  FROM (SELECT 
+                    FROM (SELECT 
                             "MAL_ID",
                             TRIM(REPLACE(DECODE(INSTR("Aired",'to'),0,"Aired",SUBSTR("Aired",0,INSTR("Aired",'to')-1)),',')) AS DATA_INICIAL,
                             TRIM(REPLACE(DECODE(INSTR("Aired",'to'),0,NULL,SUBSTR("Aired",INSTR("Aired",'to')+2)),',')) AS DATA_FINAL
-                         FROM CARGA_ANIME))
+                            FROM SYSTEM.CG_ANIMES))
         LOOP
             VREGISTRO.DT_INICIAL := I.DATA_INICIAL; 
-            VREGISTRO.DT_FINAL := I.DATA_FINAL;
-            VREGISTRO.MAL_ID := I."MAL_ID";
+            VREGISTRO.DT_FINAL   := I.DATA_FINAL;
+            VREGISTRO.MAL_ID     := I."MAL_ID";
             PIPE ROW(VREGISTRO);
-             
         END LOOP;
-     END; 
-     
-     PROCEDURE SP_DATAS_INICIAIS_FINAIS AS 
-     BEGIN
+        END; 
+        
+        PROCEDURE SP_DATAS_INICIAIS_FINAIS AS 
+    -- Atualiza as datas finais e inicias dos animes
+        BEGIN
         FOR I IN (SELECT A.DT_INICIAL,
-                         A.DT_FINAL,
-                         A.MAL_ID 
-                    FROM SYSTEM.PKG_VALIDA_CARGA_ANIME.FNC_VALIDA_DATAS_INICIAIS_FINAIS(9) A 
-                  JOIN CARGA_ANIME B ON A.MAL_ID = B."MAL_ID")
+                            A.DT_FINAL,
+                            A.MAL_ID 
+                    FROM TABLE(PKG_VALIDA_CARGA.FNC_VALIDA_DATAS_INICIAIS_FINAIS) A 
+                    JOIN SYSTEM.CG_ANIMES B ON A.MAL_ID = B."MAL_ID")
         LOOP
-            UPDATE CARGA_ANIME A 
-             SET A.DATA_INICIAL = I.DT_INICIAL,
-                 A.DATA_FINAL   = I.DT_FINAL
-            WHERE A."MAL_ID"  = I.MAL_ID;
+            UPDATE SYSTEM.CG_ANIMES A 
+                SET A.DATA_INICIAL = I.DT_INICIAL,
+                    A.DATA_FINAL   = I.DT_FINAL
+                WHERE A."MAL_ID"  = I.MAL_ID;
         END LOOP;
         COMMIT;
-     END;
+        END;
     
 END;
--- etapas_a_parte.sql
+++ etapas_a_parte.sql
@@ -1,4 +1,15 @@
 -- Adicionando novos campos na tabela de carga_anime
-ALTER TABLE CARGA_ANIME
+ALTER TABLE CG_ANIMES
 ADD (DATA_INICIAL DATE,
      DATA_FINAL DATE);
+
+-- Execultando as procedures e functinos da package
+EXEC PKG_VALIDA_CARGA.SP_VALIDA_VALORES_NULO;
+EXEC PKG_VALIDA_CARGA.SP_DATAS_INICIAIS_FINAIS;
+SELECT * FROM TABLE(PKG_VALIDA_CARGA.FNC_VALIDA_DATAS_INICIAIS_FINAIS);
+
+
+-- atualizando valores 
+UPDATE CG_ANIMES
+    SET "Score" = NULL
+WHERE "Score" LIKE '%"%';
